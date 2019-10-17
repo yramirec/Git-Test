527c568e70fb7c2fe3d945853e72695308422b40
Alvaro Manuel Valdivia Palomino <avaldivp@everis.com>
2019-10-17 16:59:22 -0500

    merge?

--- src/main/java/com/everis/controller/StudentController.java
+++ src/main/java/com/everis/controller/StudentController.java
@@ -1,8 +1,11 @@
 package com.everis.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("api/v1.0")
 public class StudentController {
 	
 	public StudentController() {
-		
+		//metodo controlador
 	};
 }
